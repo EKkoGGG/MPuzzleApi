8103d0e42a7349a49066aebb16646537217746a0
JJ <330564052@qq.com>
2020-01-14 18:19:30 +0800

    增加异步获得故事题目接口

--- MPuzzleApi/Controllers/StoryController.cs
+++ MPuzzleApi/Controllers/StoryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MPuzzleApi.Models;
+using Newtonsoft.Json;
 
 namespace MPuzzleApi.Controllers
 {
@@ -19,6 +20,29 @@ namespace MPuzzleApi.Controllers
         {
             _context = context;
         }
+
+
+        // GET: api/Story/Title
+        //[HttpGet("Title")]
+        //public async Task<IActionResult> GetStoryTitle()
+        //{
+        //    //return await _context.Story.Select(t => new { Id = t.Id, t.Title }).ToListAsync();
+        //    return await 
+        //}
+
+        [HttpGet("Title")]
+        public async Task<IActionResult> GetStoryTitleAsync()
+        {
+            List<Story> story = await _context.Story.ToListAsync();
+            return new JsonResult(
+                 story.Select(t => new
+                {
+                    t.Id,
+                    t.Title
+                 })
+            );
+        }
+
 
         // GET: api/Story
         [HttpGet]
@@ -41,69 +65,74 @@ namespace MPuzzleApi.Controllers
             return story;
         }
 
-        // PUT: api/Story/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutStory(int id, Story story)
-        {
-            if (id != story.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(story).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StoryExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Story
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<Story>> PostStory(Story story)
-        {
-            _context.Story.Add(story);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetStory", new { id = story.Id }, story);
-        }
-
-        // DELETE: api/Story/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Story>> DeleteStory(int id)
-        {
-            var story = await _context.Story.FindAsync(id);
-            if (story == null)
-            {
-                return NotFound();
-            }
-
-            _context.Story.Remove(story);
-            await _context.SaveChangesAsync();
-
-            return story;
-        }
-
         private bool StoryExists(int id)
         {
             return _context.Story.Any(e => e.Id == id);
         }
+
+        #region 屏蔽其他请求
+
+        //// PUT: api/Story/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        //// more details see https://aka.ms/RazorPagesCRUD.
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutStory(int id, Story story)
+        //{
+        //    if (id != story.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(story).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!StoryExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //// POST: api/Story
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        //// more details see https://aka.ms/RazorPagesCRUD.
+        //[HttpPost]
+        //public async Task<ActionResult<Story>> PostStory(Story story)
+        //{
+        //    _context.Story.Add(story);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetStory", new { id = story.Id }, story);
+        //}
+
+        //// DELETE: api/Story/5
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<Story>> DeleteStory(int id)
+        //{
+        //    var story = await _context.Story.FindAsync(id);
+        //    if (story == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Story.Remove(story);
+        //    await _context.SaveChangesAsync();
+
+        //    return story;
+        //}
+
+        #endregion
+
     }
 }
